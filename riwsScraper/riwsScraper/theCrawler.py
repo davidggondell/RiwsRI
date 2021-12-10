b00ff2c64d02c07d5c01703f54ffc687816db317
David García Gondell <david.ggondell@udc.es>
2021-12-10 23:28:49 +0100

    removed outdated files

--- riwsScraper/riwsScraper/theCrawler.py
+++
@@ -1,9 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
-process = CrawlerProcess(get_project_settings())
-
-process.crawl('dia')
-process.crawl("diaSupermarket")
-
-process.start()
